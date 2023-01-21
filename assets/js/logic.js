58a2bad4a909dc9dab8cdaf7abb5a824237abc81
Parsa Hemmati <parsa.hemmati@gmail.com>
2023-01-21 20:42:13 +0000

    makes start button disapper upon clicking and logs hi in consolegit push origin main

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// creates initial variables
+let currentQuestion = 0;
+let score = 0;
+let timer = 60;
+
+document.getElementById("start").addEventListener("click", function () {
+    document.getElementById("start").style.display = "none";
+    console.log("Hi!")
+    // displayQuestion();
+    // startTimer();
+})
