5d8fe49c666f53b07979f89948cea0de09120a24
Parsa Hemmati <parsa.hemmati@gmail.com>
2023-01-23 23:42:52 +0000

    added showing question functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,19 +5,59 @@ let timer = 60;
 
 document.getElementById("start").addEventListener("click", function () {
     document.getElementById("start").style.display = "none";
+    document.getElementById("questions").classList.remove("hide")
     displayQuestion();
     // startTimer();
 })
 
 function displayQuestion() {
-    let question = questions[currentQuestion].question;
-    let answers = questions[currentQuestion].answers;
-    
-    document.getElementById("question-title").innerHTML = question;
-    
-    let answerButtons = "";
-    for (let key in answers) {
-      answerButtons += "<button onclick='checkAnswer(" + key + ")'>" + answers[key] + "</button>";
+    if (currentQuestion < questions.length || timer > 0) {
+        let question = questions[currentQuestion].question;
+        let answers = questions[currentQuestion].answers;
+
+        document.getElementById("question-title").innerHTML = question;
+
+        let answerButtons = "";
+        for (let key in answers) {
+            answerButtons += '<button class="choice">' + answers[key] + '</button>';
+        }
+        document.getElementById("choices").innerHTML = answerButtons;
     }
-    document.getElementById("choices").innerHTML = answerButtons;
-  }
+}
+
+document.body.addEventListener("click", function(event) {
+    if (event.target.classList.contains("choice")) {
+        if (event.target.innerHTML === questions[currentQuestion].correctAnswer) {
+            console.log("Correct answer!")
+            score++;
+        };
+        currentQuestion++;
+        displayQuestion();
+
+    }
+  });
+  
+
+
+
+
+
+// document.getElementsByClassName("choice").forEach(button => {
+//     button.addEventListener("click", function() {
+//       console.log("Button clicked!");
+//     });
+//   });
+  
+
+// function checkAnswer(answer) {
+    // if (answer === questions[currentQuestion].correctAnswer) {
+    //     score++;
+    // };
+
+//     if (currentQuestion === questions.length - 1 || timer === 0) {
+//         endQuiz();
+//     } else {
+//         currentQuestion++;
+//         displayQuestion();
+//     }
+// }
---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+const questions = [
+    {
+      question: "What is the syntax for creating a variable in JavaScript?",
+      answers: {
+        a: "var variableName = value;",
+        b: "new variableName = value;",
+        c: "let variableName = value;"
+      },
+      correctAnswer: "var variableName = value;"
+    },
+    {
+      question: "Which of the following is not a type of loop in JavaScript?",
+      answers: {
+        a: "for",
+        b: "while",
+        c: "forever"
+      },
+      correctAnswer: "forever"
+    },
+    {
+      question: "What is the syntax for an anonymous function in JavaScript?",
+      answers: {
+        a: "function() {}",
+        b: "() => {}",
+        c: "var functionName = function() {}"
+      },
+      correctAnswer: "() => {}"
+    },
+    {
+      question: "How can you add an element to an array in JavaScript?",
+      answers: {
+        a: "array.add(element)",
+        b: "array.push(element)",
+        c: "array += element"
+      },
+      correctAnswer: "array.push(element)"
+    },
+    {
+      question: "What is the syntax for an if-else statement in JavaScript?",
+      answers: {
+        a: "if (condition) { } else { }",
+        b: "if condition { } else { }",
+        c: "if {condition} { } else { }"
+      },
+      correctAnswer: "if (condition) { } else { }"
+    }
+  ];
+  
