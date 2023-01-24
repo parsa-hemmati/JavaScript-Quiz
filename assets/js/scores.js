234183d85cad428e0ea71bf50fc5be6b9a1edee8
Parsa Hemmati <parsa.hemmati@gmail.com>
2023-01-24 18:30:22 +0000

    creates a list of scorers

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,7 +4,7 @@ var list = document.getElementById("highscores");
 for (let i = 0; i < localStorage.length; i++) {
     var data = JSON.parse(localStorage.getItem(localStorage.key(i)));
     var item = document.createElement('li');
-    item.innerHTML = localStorage.key(i) + " has scored " + localStorage.key(i).data;
+    item.innerHTML = localStorage.key(i);
     list.appendChild(item);
 
 }
